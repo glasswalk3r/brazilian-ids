cee1119ecbbefdc09b82919cffeab3fafbea9d46
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2024-07-30 20:11:16 -0300

    chore: deprecated function

--- src/brazilian_ids/functions/util.py
+++ src/brazilian_ids/functions/util.py
@@ -1,12 +1,5 @@
-"""Helper functions and regular expressions for validating identifiers."""
+"""Helpers for validating multiple identifiers."""
 
 import re
 
-
 NONDIGIT_REGEX = re.compile(r"[^0-9]")
-
-
-def pad_id(identifier: str, fmt: str) -> str:
-    """Pad an ID with a given format."""
-    identifier = NONDIGIT_REGEX.sub("", identifier)
-    return fmt % identifier
