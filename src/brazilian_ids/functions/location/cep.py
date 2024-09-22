44be4a9243014845582fb6872316aff7cb5dfc00
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2024-09-22 20:59:30 -0300

    feat: extended evaluations for CEP

--- src/brazilian_ids/functions/location/cep.py
+++ src/brazilian_ids/functions/location/cep.py
@@ -3,6 +3,7 @@ Portuguese), which is the equivalent of zipcodes.
 
 The meaning of numeric codes for region, sub region, etc, are in strict control
 of a private company in Brazil called Correios. The company has the monopoly in
+
 Brazil and doesn't provide data besides simply queries with limited results and
 restricted by captchas to making data scraping more difficult.
 
@@ -13,12 +14,9 @@ See also:
 """
 
 from dataclasses import dataclass
+from typing import Generator
+
 from brazilian_ids.functions.exceptions import InvalidIdError
-
-
-class InvalidCepError(InvalidIdError):
-    def id_type(self, cep: str):
-        return f"Invalid CEP code '{cep}'"
 
 
 @dataclass(frozen=True, slots=True, repr=False)
@@ -72,8 +70,87 @@ class CEP:
         return a <= b
 
 
-def is_valid(cep: str, raw: bool = True, digits: int = 0) -> bool:
-    if raw:
+class Singleton(type):
+    """Implement the singleton pattern."""
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class CepInvalidStateError(ValueError):
+    """Error for CEP associated with a invalid state code."""
+    def __init__(self, state_code):
+        super().__init__(f"The state '{state_code}' does not exist")
+        self.state_code = state_code
+
+
+class CepRange(metaclass=Singleton):
+    """Representation of all the CEPs range by state, as documented by Correios."""
+    __slots__ = "__ranges"
+
+    def __init__(self):
+        self.__ranges = {
+            "SP": ("01000-000", "05999-999"),
+            "RJ": ("20000-000", "28999-999"),
+            "MG": ("30000-000", "39999-999"),
+            "BA": ("40000-000", "48999-999"),
+            "RS": ("90000-000", "99999-999"),
+            "CE": ("60000-000", "63999-999"),
+            "PE": ("50000-000", "55999-999"),
+            "PB": ("58000-000", "58999-999"),
+            "MA": ("65000-000", "65999-999"),
+            "AL": ("57000-000", "57999-999"),
+            "SE": ("49000-000", "49999-999"),
+            "GO": ("74000-000", "76999-999"),
+            "DF": ("70000-000", "71999-999"),
+            "TO": ("77000-000", "77999-999"),
+            "PA": ("66000-000", "68899-999"),
+            "AM": ("69000-000", "69299-999"),
+            "AP": ("68900-000", "68999-999"),
+            "RR": ("69300-000", "69399-999"),
+            "MT": ("78000-000", "78899-999"),
+            "MS": ("79000-000", "79999-999"),
+            "PI": ("64000-000", "64999-999"),
+            "SC": ("88000-000", "88999-999"),
+            "ES": ("29000-000", "29999-999"),
+        }
+
+    def ranges_by_state(self, state: str) -> tuple[CEP, CEP]:
+        """Return the a pair of CEPs related to a given state code.
+
+        See ``brazilian_ids.functions.location.states`` module for all valid states codes.
+        """
+        if state not in self.__ranges:
+            raise CepInvalidStateError(state)
+
+        pair = self.__ranges[state]
+        return (parse(pair[0]), parse(pair[1]))
+
+    def all_ranges(self) -> Generator[tuple[CEP, CEP], None, None]:
+        """Go through all the CEP ranges per state, returning a pair ``CEP`` instances, where the index 0 is the
+        first CEP in the range and the index 2 the last one.
+        """
+        for start, end in self.__ranges.values():
+            yield (parse(start), parse(end))
+
+    def __repr__(self):
+        return "{0}, total of ranges: {1}".format(self.__class__.__name__, len(self.__ranges))
+
+
+def is_valid(cep: str, raw: bool = False, digits: int = 0) -> bool:
+    """Check if a CEP is valid or not.
+
+    The ``raw`` parameter is a small optimization: if you're sure that the CEP string only contains numbers, then you
+    you can skip one of the steps required to validate it. If you're unsure, just use the default value.
+
+    The ``digits`` parameter is also used to skip one of the validations steps, if you already know before hand the
+    length of the ``cep`` string (for any reason). Of course, if you didn't need this information elsewhere, there is
+    use to do ``len(cep)`` before calling this function and passing the result as ``digits``.
+    """
+    if not raw:
         cep = cep.replace("-", "")
 
     if digits == 0:
@@ -81,6 +158,40 @@ def is_valid(cep: str, raw: bool = True, digits: int = 0) -> bool:
 
     expected = set([4, 5, 7, 8])
     return digits in expected
+
+
+def is_valid_extended(cep: str, raw: bool = False, digits: int = 0, state: str | None=None) -> bool:
+    """Check if a CEP is valid or not.
+
+    This function does everything that ``is_valid`` function does, plus some additional verifications that will take a
+    longer time to finish.
+
+    Those verifications are not perfect, since Correios doesn't allow anymore online verifications, but it does rely
+    on published information that is still public.
+
+    If you're are able to inform the ``state`` that you believe the CEP should be part of, then that will speed up the
+    validation process.
+    """
+    if not is_valid(cep=cep, raw=raw, digits=digits):
+        return False
+
+    ranges = CepRange()
+    candidate = parse(cep)
+
+    if state is not None:
+        start, end = ranges.ranges_by_state(state)
+        return candidate >= start and candidate <= end
+
+    for start, end in ranges.all_ranges():
+        if candidate >= start and candidate <= end:
+            return True
+
+    return False
+
+class InvalidCepError(InvalidIdError):
+    """Exception for an invalid CEP."""
+    def id_type(self, cep: str):
+        return f"Invalid CEP code '{cep}'"
 
 
 def format(cep: str) -> str:
--- tests/test_cep.py
+++ tests/test_cep.py
@@ -1,6 +1,7 @@
 import pytest
+import inspect
 
-from brazilian_ids.functions.location.cep import format, parse, CEP, is_valid
+from brazilian_ids.functions.location.cep import format, parse, CEP, is_valid, is_valid_extended, CepRange, CepInvalidStateError
 
 
 @pytest.fixture
@@ -90,6 +91,62 @@ def test_is_valid_with_valid(valid_cep):
     assert is_valid(valid_cep)
 
 
+@pytest.mark.parametrize("valid_cep", ("39880000", "39884999"))
+def test_is_valid_with_valid_and_raw(valid_cep):
+    assert is_valid(cep=valid_cep, raw=True)
+
+
 @pytest.mark.parametrize("invalid_cep", ("123", "123456", "123456789"))
 def test_is_valid_with_invalid(invalid_cep):
-    assert not is_valid(invalid_cep)
+    assert not is_valid(cep=invalid_cep, raw=False)
+
+
+@pytest.mark.parametrize("invalid_cep", ("123", "123456", "123456789"))
+def test_is_valid_extended_with_simple_invalid(invalid_cep):
+    assert not is_valid(cep=invalid_cep, raw=False)
+
+
+def test_cep_range_class():
+    assert inspect.isclass(CepRange)
+
+
+def test_cep_range_instance():
+    instance = CepRange()
+    expected = ("ranges_by_state", "all_ranges")
+
+    for method_name in expected:
+        assert inspect.ismethod(getattr(instance, method_name))
+
+
+def test_cep_range_singleton():
+    i1 = CepRange()
+    i2 = CepRange()
+    assert i1 == i2
+
+
+def test_cep_range_all_ranges():
+    instance = CepRange()
+    result = instance.all_ranges()
+    assert result.__class__.__name__ == "generator"
+    pair = next(result)
+    assert pair.__class__.__name__ == "tuple"
+
+    for i in pair:
+        assert pair[i].__class__.__name__ == "CEP"
+
+
+@pytest.mark.parametrize("invalid_cep", ("72000-000", "73000-000"))
+def test_is_valid_extended_with_complex_invalid(invalid_cep):
+    assert not is_valid_extended(cep=invalid_cep, raw=False)
+
+
+def test_is_valid_extended_with_invalid_state():
+    invalid_state = "XY"
+    with pytest.raises(CepInvalidStateError) as e:
+        is_valid_extended(cep="72000-000", state=invalid_state)
+
+    assert invalid_state in str(e.value)
+
+
+def test_is_valid_extended_with_valid_state():
+    assert is_valid_extended(cep="88100-000", state="SC")
